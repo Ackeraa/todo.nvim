2f039a5d2de273508d80a4dbcf4c9fc860669320
Ackeraa <ggaoshanxun@gmail.com>
2022-07-28 22:52:47 +0800

    fixed osx reminder bug

--- lua/todo/extensions/reminder/init.lua
+++ lua/todo/extensions/reminder/init.lua
@@ -1,6 +1,9 @@
 local reminder = {}
 
-reminder.script_path = "lua/todo/extensions/reminder/"
+reminder.script_path = function()
+   local str = debug.getinfo(2, "S").source:sub(2)
+   return str:match("(.*/)")
+end
 
 -- do not use this, because it's pretty slow.
 reminder.read_from_reminder = function(file_path)
@@ -9,7 +12,7 @@ reminder.read_from_reminder = function(file_path)
 end
 
 reminder.write_to_reminder = function(file_path)
-    local script = reminder.script_path.."write_to_reminder.scpt "
+    local script = reminder.script_path().."write_to_reminder.scpt "
     io.popen("osascript "..script..file_path)
 end
 
